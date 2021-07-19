490b539de7066a4a63458a4a090464ab7cd18efe
liyong <liyong@yit.com>
2021-07-19 17:56:45 +0800

    vite+vue+ts配置基本完成

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app">
+    <h1>一条</h1>
+  </div>
+</template>
+
+<script lang="ts">
+let lidapang = {
+  a: {
+    b: 0
+  }
+};
+console.log(lidapang?.a?.b);
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  build: {
+    target: 'es2015',
+    minify: false
+  },
+  plugins: [
+    vue()
+  ]
+})
