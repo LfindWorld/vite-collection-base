c6222e29c581cb37e541e9143b8f6c60658b69a6
liyong <liyong@yit.com>
2021-07-20 16:13:23 +0800

    优化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,6 @@
   <div class="home">
     <img class="img" alt="Vue logo" src="@/assets/images/but.jpg">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-    <router-link to="/"><el-button>Go to Home</el-button></router-link>
-     <el-row>
-      <el-button>默认按钮</el-button>
-      <el-button type="primary">主要按钮</el-button>
-      <el-button type="success">成功按钮</el-button>
-      <el-button type="info">信息按钮</el-button>
-      <el-button type="warning">警告按钮</el-button>
-      <el-button type="danger">危险按钮</el-button>
-    </el-row>
   </div>
 </template>
 
@@ -30,7 +21,12 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+  .home {
+    padding: 0 30px;
+  }
   .img {
     width: 320px;
+    display: block;
+    margin: 0 auto;
   }
 </style>
